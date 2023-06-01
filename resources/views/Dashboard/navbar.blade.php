74995d7b21e994bfc05aa9e3c4a30f91055bc971
Tasneem Safia <tasneemabusafyah@gmail.com>
2023-06-01 13:50:18 +0300

    navBar commit

--- resources/views/Dashboard/navbar.blade.php
+++ resources/views/Dashboard/navbar.blade.php
@@ -14,13 +14,27 @@
                     class="btn btn-icon btn-icon-mobile w-auto btn-clean d-flex align-items-center btn-lg px-2"
                     id="kt_quick_user_toggle">
                                 <span
-                                    class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">Hi,
-                                    {{Auth::user()->name .' - '. Auth::user()->guard }}</span>
+                                    class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">
+                                    Hi, {{Auth::user()->name .' - '. Auth::user()->guard }}</span>
                     <span
                         class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3"></span>
                     <span class="symbol symbol-lg-35 symbol-25 symbol-light-success">
 										</span>
                 </div>
+                @if(\Illuminate\Support\Facades\Auth::user()->unique_id)
+                    <div
+                        class="btn btn-icon btn-icon-mobile w-auto btn-clean d-flex align-items-center btn-lg px-2"
+                        id="kt_quick_user_toggle">
+                                <span
+                                    class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">
+                                    ID: {{Auth::user()->unique_id }}</span>
+                        <span
+                            class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3"></span>
+                        <span class="symbol symbol-lg-35 symbol-25 symbol-light-success">
+										</span>
+                    </div>
+                @endif
+
             </div>
             <!--end::User-->
         </div>
